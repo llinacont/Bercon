1d8fa604d8e68d3f58c497bd2f7af99223ec039e
Carlos <carlos@carlos-VPCSE2C5E.(none)>
2013-01-13 23:45:28 +0100

    a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  def after_sign_in_path_for(resource_or_scope)
-    product_path(resource_or_scope)
+  def after_sign_in_path_for(resource)
+    products_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Bercon::Application.routes.draw do
   devise_for :users do
     
-    resources :products
-     
-    resources :clients
+  resources :products
+    
+  resources :clients 
   
   end
    
