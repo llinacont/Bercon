23e33117f4f6e3e1c4f93b56a69e94183f384a40
Carlos <carlos@carlos-VPCSE2C5E.(none)>
2013-01-15 12:58:43 +0100

    a

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -44,7 +44,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item, notice: 'Line item was successfully created.' }
+        format.html { redirect_to :back, notice: 'Line item was successfully created.' }
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
