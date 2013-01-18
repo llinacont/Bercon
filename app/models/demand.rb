9c54b27c031853c2b627c16053180d82e9f82704
Carlos <carlos@carlos-VPCSE2C5E.(none)>
2013-01-18 23:53:09 +0100

    a

--- app/models/demand.rb
+++ app/models/demand.rb
@@ -1,5 +1,6 @@
 class Demand < ActiveRecord::Base
   belongs_to :client
   has_many :line_items, :dependent => :destroy
-  attr_accessible :client_id
+  attr_accessible :client_id, :update_at
+  attr_accessor :update_at
 end
