a4a748abc433dee603231199c2d97b4bcf014435
Carlos <carlos@carlos-VPCSE2C5E.(none)>
2013-01-23 12:35:59 +0100

    a

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130114170158) do
+ActiveRecord::Schema.define(:version => 20130123092721) do
 
   create_table "clients", :force => true do |t|
     t.string   "name"
@@ -31,8 +31,9 @@ ActiveRecord::Schema.define(:version => 20130114170158) do
   create_table "line_items", :force => true do |t|
     t.integer  "product_id"
     t.integer  "demand_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
+    t.integer  "quantity",   :default => 1
   end
 
   create_table "products", :force => true do |t|
