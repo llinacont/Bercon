a2fc7595e924e170cd5995458f644f041000530f
Carlos <carlos@carlos-VPCSE2C5E.(none)>
2013-03-21 20:24:32 +0100

    a

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(:version => 20130319193110) do
     t.datetime "created_at",                       :null => false
     t.datetime "updated_at",                       :null => false
     t.integer  "user_id"
-    t.string   "state",      :default => "creado"
+    t.string   "state"    
   end
 
   create_table "events", :force => true do |t|
